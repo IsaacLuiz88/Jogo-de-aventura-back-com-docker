b047ba40a89847b4d2f0bfc95cd66b4a3e5dd20e
Isaac <isaacluiz0808@gmail.com>
2025-03-04 15:57:17 -0300

    Ajustando o database.js para verificação de erro e ajustando as credenciais para um .env; Adicionando ao .gitignore um .env;

--- database.js
+++ database.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 export function connectToDatabase() {
-    const uri = 'mongodb+srv://Livia:livia123@cluster0.n7cnp.mongodb.net/Jogo-Aventura?retryWrites=true&w=majority&appName=Cluster0'
-
+    const uri = process.env.MONGO_URL;
+    
+    if(!uri) {  // Se a URI não foi definida, exibe um erro
+        console.error('URI de conexão com o MongoDB não definida');
+        process.exit(1);
+    }
+    
     mongoose.connect(uri)
-    .then(() => console.log('conectado ao banco'))
-    .catch((e) => console.error('erro ao conectar', e))
+    .then(() => console.log('✅ Conectado ao MongoDB Atlas'))
+    .catch((e) => console.error('❌ Erro ao conectar ao MongoDB', e))
 }
